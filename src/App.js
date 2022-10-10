ac34fa47253c999d052f2bf6670d9578eac295d3
kingssik <hyunsikkang25@gmail.com>
2022-10-10 21:09:19 +0900

    array, object state 변경하는 법을 이용한 글수정 버튼 추가

--- src/App.js
+++ src/App.js
@@ -1,29 +1,39 @@
+/* eslint-disable */
+
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
 function App() {
 
-  // let [글제목1, b] = useState('남자 코트 추천');
-  // let [글제목2, c] = useState('강남 우동맛집');
-  // let [글제목3, d] = useState('파이썬 독학');
-
-  // array를 이용한 방법
-  let [글제목, b] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬 독학']);
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬 독학']);
+  let [따봉, 따봉변경] = useState(0);
 
   return (
+
     <div className="App">
       <div className="black-nav">
         <h4>ReactBlog</h4>
       </div>
+
+      <button onClick={()=>{
+        let copy = [...글제목];
+        copy[0] = '여자 코트 추천';
+        글제목변경(copy);
+      }}>글수정</button>
+
+      
+
       <div className='list'>
-        <h4>{글제목[0]}</h4>
+        <h4>{글제목[0]} <span onClick={ () => { 따봉변경(따봉+1) }}>👍</span> {따봉} </h4>
         <p>10월 9일 발행</p>
       </div>
+
       <div className='list'>
         <h4>{글제목[1]}</h4>
         <p>10월 9일 발행</p>
       </div>
+
       <div className='list'>
         <h4>{글제목[2]}</h4>
         <p>10월 9일 발행</p>
