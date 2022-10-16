b7d93a6dd6639858f6777be1fe8ae9884eb6a610
kingssik <hyunsikkang25@gmail.com>
2022-10-16 18:59:17 +0900

    map을 이용한 따봉 수 개별적 분리

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ import { useState } from 'react';
 function App() {
 
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬 독학']);
-  let [따봉, 따봉변경] = useState(0);
+  let [따봉, 따봉변경] = useState([0,0,0]);
+  // let [따봉1, 따봉변경1] = useState(0);
+  // let [따봉2, 따봉변경2] = useState(0);
   let [modal, setModal] = useState(false);
 
   return (
@@ -18,25 +20,31 @@ function App() {
       </div>
      
       {/* <div className='list'>
-        <h4>{글제목[0]} <span onClick={ () => { 따봉변경(따봉+1) }}>👍</span> {따봉} </h4>
+        <h4 onClick={()=>{ setModal(!modal) }}>{글제목[0]} <span onClick={ () => { 따봉변경(따봉[0]+1) }}>👍</span> {따봉[0]} </h4>
         <p>10월 9일 발행</p>
       </div>
 
       <div className='list'>
-        <h4>{글제목[1]}</h4>
+        <h4 onClick={()=>{ setModal(!modal) }}>{글제목[1]} <span onClick={ () => { 따봉변경(따봉[1]+1) }}>👍</span> {따봉[1]} </h4>
         <p>10월 9일 발행</p>
       </div>
 
       <div className='list'>
-        <h4 onClick={()=>{ setModal(!modal) }}>{글제목[2]}</h4>
+        <h4 onClick={()=>{ setModal(!modal) }}>{글제목[2]} <span onClick={ () => { 따봉변경(따봉[2]+1) }}>👍</span> {따봉[2]} </h4>
         <p>10월 9일 발행</p>
       </div> */}
       
       {
         글제목.map(function(a, i){
+
           return (
             <div className='list' key={i}>
-              <h4 onClick={()=>{ setModal(!modal) }}>{ 글제목[i] } <span onClick={ () => { 따봉변경(따봉+1) }}>👍</span> {따봉}</h4>
+              <h4 onClick={()=>{ setModal(!modal) }}>{ 글제목[i] } 
+                <span onClick={ () => {
+                  let copy =[...따봉];
+                  copy[i] = copy[i] + 1;
+                  따봉변경(copy) 
+                  }}>👍</span> {따봉[i]}</h4>
               <p>10월 9일 발행</p>
             </div>
           )
